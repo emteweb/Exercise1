de09c4b65994c72044ba5b5fee92bac7c61922fd
Marcin Trebacz <emteweb@gmail.com>
2020-06-17 18:28:40 +0200

    initial commit - add index.html and scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// funkcja anonimowa
+(function(){
+    var zmienna = 0;
+    //console.log("Pierwsza funkcja")
+})();
+
+var container = document.querySelector("#container");
+var p = document.querySelectorAll("p");
